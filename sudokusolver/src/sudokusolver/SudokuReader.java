7e62ce2489b03f42ef849e1ab87992cb0d5f7133
OscarBli <oscar99.oscar99@hotmail.se>
2020-12-07 12:12:34 +0100

    nn

--- sudokusolver/src/sudokusolver/SudokuReader.java
+++ sudokusolver/src/sudokusolver/SudokuReader.java
@@ -16,12 +16,14 @@ public class SudokuReader implements SudokuSolver {
 
 	@Override
 	public void setCell(int row, int col, int val) throws IllegalArgumentException {
-		
+		board[row-1][col-1] = val;
 	}
 
 	@Override
 	public int getCell(int row, int col) throws IllegalArgumentException {
 	// TODO Auto-generated method stub
+		
+		
 		return 0;
 	}
 
